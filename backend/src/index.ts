439aa820e41613ed51caa0f365a121db4cf6600d
Avelyn Choi <avelynhc@gmail.com>
2023-05-22 17:54:01 -0400

    updated callback function to retrieve user information from google profile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,46 @@
 import express from "express";
-import {OAuth2Client} from "google-auth-library";
+import { OAuth2Client } from "google-auth-library";
 import dotenv from "dotenv";
+import url from "url";
+import { getUser } from "./api";
 
 dotenv.config();
 const index = express();
 
+const oAuth2Client:OAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URIS
+);
+
+index.get("/", (req, res) => {
+   res.send({message: "Welcome to mini-games website!"});
+});
+
+index.get("/favicon.ico", (req, res) => {
+});
+
 index.get("/login", (req, res) => {
-    const oAuth2Client = new OAuth2Client(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        process.env.REDIRECT_URIS
-    );
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
+    const authorizeUrl:string = oAuth2Client.generateAuthUrl({
         scope: "https://www.googleapis.com/auth/userinfo.profile",
     });
     res.redirect(authorizeUrl)
 });
 
-index.get("/callback", (req, res) => {
-    res.json({ message: "redirect to the browser with code as a query string!" });
+index.get("/callback", async (req, res) => {
+    const queryString = new url.URL(req.url, 'http://localhost:3000')
+        .searchParams;
+    const code: string = queryString.get('code') || "";
+    const tokenRes = await oAuth2Client.getToken(code);
+    oAuth2Client.setCredentials(tokenRes.tokens);
+    console.info({token: tokenRes.tokens});
+    console.info("Authentication successful!");
+    const accessToken: any = tokenRes.tokens.access_token;
+    const userInfo:any = await getUser(accessToken);
+    res.json(userInfo);
 });
 
-const port = 4000;
+const port:number = 4000;
 index.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 })
