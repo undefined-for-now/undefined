d394b0d4a766469696516ffe20837abc6773a34c
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2023-05-28 23:56:56 -0400

    Created recently games cards

--- game/src/App.js
+++ game/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Navbar2 from "./components/Navbar2";
+import RecentlyCards from "./components/RecentlyCards";
 
 
 function App() {
   return (
     <div>
     <Navbar2/> 
+    <RecentlyCards/>
     </div>
   );
 }
