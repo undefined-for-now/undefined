685e902f9f940f92abc66939a3e1528b4d6cc3a2
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2023-05-28 23:46:27 -0400

    modified navbar

--- game/src/App.js
+++ game/src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
+import Navbar2 from "./components/Navbar2";
 
 
 function App() {
   return (
-    <>
-     <Navbar/>
-    </>
+    <div>
+    <Navbar2/> 
+    </div>
   );
 }
 
