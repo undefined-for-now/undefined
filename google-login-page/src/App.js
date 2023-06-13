e0806a4a4ccc609926d9e91723c5a6598d94bd33
Avelyn Choi <avelynhc@gmail.com>
2023-06-13 15:47:57 -0400

    Removed duplicate google-login-page folder

--- google-login-page/src/App.js
+++
@@ -1,8 +0,0 @@
-import AuthPage from "./pages/AuthPage";
-import { Route, Routes } from "react-router-dom";
-
-const App = () => {
-  return <AuthPage />
-}
-
-export default App;
--- google-login-page/src/pages/AuthPage.js
+++
@@ -1,8 +0,0 @@
-
-const AuthPage = () => {
-    return (
-        <a href="http://127.0.0.1:4000/login">Google Login</a>
-    )
-}
-
-export default AuthPage;
