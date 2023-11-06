ad82e7ca507b17d024dfbf0e8dab8a02b6730314
문효안 <hyoanmoon@munhyoacbookair.home>
2023-11-05 23:17:30 -0500

    put sample hangman game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import GamesCards from "./components/GamesCards";
 import Home from "./components/Home";
 import Game from "./components/Game";
-import Navbar from "./components/Navbar";
 import Navbar2 from "./components/Navbar2";
+
 import RecentlyCards from "./components/RecentlyCards";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="max-w-[1640px] center-screen">
         <Navbar2 />
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:gameId" element={<Game />} />
         </Routes>
+        
       </div>
     </BrowserRouter>
   );
